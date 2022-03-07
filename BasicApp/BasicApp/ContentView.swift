6095523e64c4678057d0cdaf487cefab947b3389
LeoAndo <leo.777.info@gmail.com>
2022-03-07 22:05:03 +0900

    bottombarの追加

--- BasicApp/BasicApp/ContentView.swift
+++ BasicApp/BasicApp/ContentView.swift
@@ -9,8 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        TabView {
+            HomeView().tabItem() {
+                Image(systemName: "house.fill")
+                Text("Home")
+            }
+            TodoView().tabItem() {
+                Image(systemName: "pencil.circle.fill")
+                Text("Todo")
+            }
+            SeachView().tabItem() {
+                Image(systemName: "magnifyingglass.circle.fill")
+                Text("Search")
+            }
+            
+        }
     }
 }
 
---
+++ BasicApp/BasicApp/Views/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  BasicApp
+//
+//  Created by LeoAndo on 2022/03/07.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Hello, Home!")
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
