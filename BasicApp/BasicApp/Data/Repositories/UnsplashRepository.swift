40bba3d8d63dd59423e1af56e479827630d3b456
LeoAndo <leo.777.info@gmail.com>
2022-03-08 19:50:54 +0900

    add SDWebImageSwiftUI library

---
+++ BasicApp/BasicApp/Data/Repositories/UnsplashRepository.swift
@@ -0,0 +1,30 @@
+//
+//  UnsplashRepository.swift
+//  BasicApp
+//
+//  Created by LeoAndo on 2022/03/08.
+//
+
+import Combine
+
+protocol UnsplashRepository: AnyObject {
+//    typealias Response = ""
+//    typealias Error = ""
+//
+//    func searchPhotos(
+//        query: String,
+//        page: Int = 1,
+//        perPage: Int = 10,
+//        completion: @escaping (Result<Response, Error> -> Void)
+//    )
+//
+//    func searchPhotos(
+//        query: String,
+//        page: Int = 1,
+//        perPage: Int = 10,
+//    ) -> AnyPublisher<Response, Error>
+}
+
+final class UnsplashRepositoryImpl: UnsplashRepository {
+    
+}
