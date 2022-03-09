37374e57947de215a600684f6961a888023746a8
LeoAndo <leo.777.info@gmail.com>
2022-03-09 21:05:09 +0900

    add capture

--- BasicApp/BasicApp/Data/API/UnsplashServiceInterceptor.swift
+++ BasicApp/BasicApp/Data/API/UnsplashServiceInterceptor.swift
@@ -11,7 +11,7 @@ import Resolver
 final class UnsplashServiceInterceptor: RequestInterceptor {
     func adapt(_ urlRequest: URLRequest, for session: Session, completion: @escaping (Result<URLRequest, Error>) -> Void) {
         var request = urlRequest
-        let API_KEY = "TODO INPUT HERE"
+        let API_KEY = "-QVBMwGFoRbxRA5HgaFT9Q4-RctuHro48hhga5yNKY0"
         request.headers.add(name: "Accept-Version", value: "v1")
         request.headers.add(name: "Authorization", value: "Client-ID \(API_KEY)")
         completion(.success(request))
